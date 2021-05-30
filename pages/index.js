41d30301cbf9059d6b3c83c092ce3cd2e39b6a2d
JrzenonDev <jrobertoonb@gmail.com>
2021-05-30 02:02:57 -0300

    chore: fazendo requisição a api e listando o intes de pokemons

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokedex/2/')
+      .then((respostaDoServer) => {
+        if(respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+      })
+      .then((respostaEmObjeto) => {
+        console.log(respostaEmObjeto.pokemon_entries);
+        setPokemons(respostaEmObjeto.pokemon_entries);
+
+      })
+  }, []);
+
   return (
-    <div>Pokédex - JrDev</div>
+    <div>
+      Pokédex - JrDev
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.entry_number}>
+            {pokemon.pokemon_species.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
